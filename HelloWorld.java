97c8f23e5ead3d84fdf7a66f72000a9c72db4d37
jitu590 <47721343+jitu590@users.noreply.github.com>
2019-05-04 14:33:07 +0530

    create object

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,11 @@
 public class HelloWorld{
 public static void main(String []args){
+  //create object
+  HelloWorld hw=new HelloWorld();
   
     System.out.println("This is simple prog");
   System.out.println("hi");
+  
   
 }
 }
