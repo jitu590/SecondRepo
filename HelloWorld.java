85627d063d4bb50f2903d062db3af826efc29d58
jitu590 <47721343+jitu590@users.noreply.github.com>
2019-05-04 10:05:48 +0530

    add 2nd sopl

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld{
 public static void main(String []args){
-  System.out.println("This is simple java programe");
+  
+    System.out.println("This is simple prog");
+  System.out.println("hi");
+  
 }
 }
